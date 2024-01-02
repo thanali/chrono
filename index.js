ef15f43525cac40e6ec663433d0fef013ec0e5fc
lithana <lbn.nathalie@gmail.com>
2024-01-02 19:46:19 +0100

    Add Chrono Function

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 let sec = 0
+let min = 0
+let hour = 0
 
-let time = document.getElementById("time")
+const time = document.getElementById("time")
 
-let chrono = window.setInterval(tic, 1000)
+const chrono = window.setInterval(tic, 1000)
 
 function tic() {
+  sec = parseInt(sec)
+  min = parseInt(min)
+  hour = parseInt(hour)
+
   sec++
-  time.innerHTML = sec
+
   if (sec === 60) {
     sec = 0
+    min++
   }
+  if (min === 60) {
+    hour++
+    min = 0
+  }
+
+  if (sec < 10) {
+    sec = "0" + sec
+  }
+  if (min < 10) {
+    min = "0" + min
+  }
+  if (hour < 10) {
+    hour = "0" + hour
+  }
+
+  time.innerHTML = `${hour}:${min}:${sec}`
 }
